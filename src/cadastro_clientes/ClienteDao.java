fb69336f5b32ec6443f470d95f8f4e382741d16c
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-06-02 22:10:22 +0000

    herança e interfaces

---
+++ src/cadastro_clientes/ClienteDao.java
@@ -0,0 +1,24 @@
+package cadastro_clientes;
+
+public class ClienteDao implements CadastradorDeClientes {
+
+	@Override
+	public void cadastrarAtacado(ClienteAtacado ca) {
+		
+		
+	}
+
+	@Override
+	public void cadastrarVarejo(ClienteVarejo cv) {
+		
+		
+	}
+
+	@Override
+	public void cadastrarAtacarejo(ClienteAtacarejo ca) {
+		
+		
+	}
+
+	
+}
---
+++ src/cadastro_clientes/ClienteVarejo.java
@@ -0,0 +1,5 @@
+package cadastro_clientes;
+
+public class ClienteVarejo extends Cliente {
+
+}
