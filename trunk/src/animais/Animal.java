8c15f49424b037cc68bdb8091f45fdf3c4175b41
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-06-07 21:10:13 +0000

    adição de um atributo na classe animal

--- trunk/src/animais/Animal.java
+++ trunk/src/animais/Animal.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public abstract class Animal implements Serializable{
 	public String nome;
+	public Integer peso;
 	
 	public void comer() {
 		System.out.println("Animal comendo");
