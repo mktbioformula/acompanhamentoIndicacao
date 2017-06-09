a7df26129436a9c0c779fd684c9d67c1d3f9b182
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-06-09 21:49:44 +0000

    tela de CRUD de pessoa

--- trunk/src/view/CadastroPessoaView.java
+++ trunk/src/view/CadastroPessoaView.java
@@ -1,84 +1,98 @@
 package view;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
-import bean.Pessoa;
-import controle.PessoaBC;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-
-public class CadastroPessoaView extends JFrame implements ActionListener {
+public class CadastroPessoaView extends JFrame {
 	
-	
-	private JTextField nome;
-	private JTextField fone;
-	private JTextField email;
-	
-	
-	public CadastroPessoaView(){
-		this.getContentPane().setLayout(null);
-		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(66, 65, 70, 15);
-		getContentPane().add(lblNome);
-		
-		nome = new JTextField();
-		nome.setBounds(146, 63, 114, 19);
-		getContentPane().add(nome);
-		nome.setColumns(10);
-		
-		fone = new JTextField();
-		fone.setBounds(146, 123, 114, 19);
-		getContentPane().add(fone);
-		fone.setColumns(10);
-		
-		JLabel lblFone = new JLabel("Fone");
-		lblFone.setBounds(66, 125, 70, 15);
-		getContentPane().add(lblFone);
-		
-		email = new JTextField();
-		email.setBounds(146, 184, 114, 19);
-		getContentPane().add(email);
-		email.setColumns(10);
-		
-		JLabel lblEmail = new JLabel("E-mail");
-		lblEmail.setBounds(66, 186, 70, 15);
-		getContentPane().add(lblEmail);
-		
-		JButton btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.addActionListener(this);
-		btnCadastrar.setBounds(143, 250, 117, 25);
-		getContentPane().add(btnCadastrar);
-		this.setBounds(300, 300, 600, 453);
-		
+	private JComboBox pessoas;
+	 private JButton btnSelecionar;
+	 private JTextField nome;
+	 private JTextField fone;
+	 private JTextField email;
+	 private JTextField id;
+	 private JTextField cpf;
+	 
+	 public CadastroPessoaView() {
+	  getContentPane().setLayout(null);
+	  this.setBounds(300, 300, 600, 453);
+	  
+	  JLabel lblNome;
+	  JLabel lblPessoasCadastradas = new JLabel("Pessoas Cadastradas");
+	  lblPessoasCadastradas.setBounds(10, 32, 122, 14);
+	  getContentPane().add(lblPessoasCadastradas);
+	  
+	  pessoas = new JComboBox();
+	  pessoas.setBounds(142, 26, 262, 27);
+	  getContentPane().add(pessoas);
+	  
+	  btnSelecionar = new JButton("Selecionar");
+	  btnSelecionar.setBounds(414, 28, 89, 25);
+	  getContentPane().add(btnSelecionar);
+	  
+	  lblNome = new JLabel("Nome");
+	  lblNome.setBounds(69, 162, 46, 14);
+	  getContentPane().add(lblNome);
+	  
+	  nome = new JTextField();
+	  nome.setBounds(121, 159, 323, 20);
+	  getContentPane().add(nome);
+	  nome.setColumns(10);
+	  
+	  JSeparator separator = new JSeparator();
+	  separator.setBounds(34, 111, 469, 7);
+	  getContentPane().add(separator);
+	  
+	  JLabel lblFone = new JLabel("Fone");
+	  lblFone.setBounds(69, 236, 46, 14);
+	  getContentPane().add(lblFone);
+	  
+	  fone = new JTextField();
+	  fone.setBounds(121, 233, 152, 20);
+	  getContentPane().add(fone);
+	  fone.setColumns(10);
+	  
+	  JLabel lblEmail = new JLabel("Email");
+	  lblEmail.setBounds(69, 278, 46, 14);
+	  getContentPane().add(lblEmail);
+	  
+	  email = new JTextField();
+	  email.setBounds(121, 275, 323, 20);
+	  getContentPane().add(email);
+	  email.setColumns(10);
+	  
+	  JLabel lblId = new JLabel("ID");
+	  lblId.setBounds(69, 129, 46, 14);
+	  getContentPane().add(lblId);
+	  
+	  id = new JTextField();
+	  id.setBounds(121, 124, 63, 20);
+	  getContentPane().add(id);
+	  id.setColumns(10);
+	  
+	  JLabel lblCpf = new JLabel("CPF");
+	  lblCpf.setBounds(69, 199, 46, 14);
+	  getContentPane().add(lblCpf);
+	  
+	  cpf = new JTextField();
+	  cpf.setBounds(121, 190, 152, 20);
+	  getContentPane().add(cpf);
+	  cpf.setColumns(10);
+	  
+	  JButton btnExcluir = new JButton("Excluir");
+	  btnExcluir.setBounds(204, 125, 89, 23);
+	  getContentPane().add(btnExcluir);
+	  
+	  JButton btnSalvar = new JButton("Salvar");
+	  btnSalvar.setBounds(160, 333, 89, 23);
+	  getContentPane().add(btnSalvar);
+	  
+	  JButton btnUpdate = new JButton("Update");
+	  btnUpdate.setBounds(278, 333, 89, 23);
+	  getContentPane().add(btnUpdate);
+	 }
 	}
-
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		Pessoa p = new Pessoa();
-		
-		p.setNome(this.nome.getText());
-		p.setTelefone(this.fone.getText());
-		p.setEmail(this.email.getText());
-		
-		
-		PessoaBC pbc = new PessoaBC();
-		if(pbc.cadastrar(p)){
-			JOptionPane.showMessageDialog(null, "Gravado com sucesso!");
-		}else{
-			JOptionPane.showMessageDialog(null, "Erro ao gravar pessoa!");
-		}
-		
-	}
-
-
-	
-}
