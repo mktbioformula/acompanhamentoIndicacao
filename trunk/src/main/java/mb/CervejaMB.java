72ede51616ea7a4c023639a4c167a918148267c1
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-06-22 00:41:45 +0000

    exemplo finalizado

--- trunk/src/main/java/mb/CervejaMB.java
+++ trunk/src/main/java/mb/CervejaMB.java
@@ -1,5 +1,7 @@
 package mb;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
@@ -11,11 +13,23 @@ import controle.impl.CervejaBC;
 public class CervejaMB {
 	private CervejaBCI controle;
 	private Cerveja bean;
+	private List<Cerveja> list;
+	
 	
 	@PostConstruct
 	public void init(){
 		this.controle = new CervejaBC();
 		this.bean = new Cerveja();
+		this.list = controle.select();
+	}
+
+	
+	public List<Cerveja> getList() {
+		return list;
+	}
+
+	public void setList(List<Cerveja> list) {
+		this.list = list;
 	}
 
 	public Cerveja getBean() {
@@ -28,6 +42,7 @@ public class CervejaMB {
 	public void gravar(){
 		this.controle.insert(this.bean);
 		this.bean = new Cerveja();
+		this.list = controle.select();
 	}
 	
 }
