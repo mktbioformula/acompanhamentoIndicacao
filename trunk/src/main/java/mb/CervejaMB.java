5bf43cdb469fae0e92d78e43e0af32b63d32e621
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-06-23 22:51:34 +0000

    adição do PrimeFaces no projeto

--- trunk/src/main/java/mb/CervejaMB.java
+++ trunk/src/main/java/mb/CervejaMB.java
@@ -3,7 +3,9 @@ package mb;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import bean.Cerveja;
 import controle.esp.CervejaBCI;
@@ -14,16 +16,14 @@ public class CervejaMB {
 	private CervejaBCI controle;
 	private Cerveja bean;
 	private List<Cerveja> list;
-	
-	
+
 	@PostConstruct
-	public void init(){
+	public void init() {
 		this.controle = new CervejaBC();
 		this.bean = new Cerveja();
 		this.list = controle.select();
 	}
 
-	
 	public List<Cerveja> getList() {
 		return list;
 	}
@@ -35,19 +35,18 @@ public class CervejaMB {
 	public Cerveja getBean() {
 		return bean;
 	}
+
 	public void setBean(Cerveja bean) {
 		this.bean = bean;
 	}
-	
-	public void gravar(){
+
+	public void gravar() {
 		this.controle.insert(this.bean);
 		this.bean = new Cerveja();
 		this.list = controle.select();
+
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage("Ok", "Cerveja cadastrada com sucesso! "));
 	}
-	
+
 }
-
-
-
-
-
