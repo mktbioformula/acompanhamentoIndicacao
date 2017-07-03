97e84e18133cbc8e0e077b015165c5db8e3e9c0f
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-03 22:01:44 +0000

    projeto do curso 3way - Primeiro commit

---
+++ src/main/java/bean/Usuario.java
@@ -0,0 +1,50 @@
+package bean;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String nome;
+	private String email;
+	private String senha;
+	private Integer tipo;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Integer getTipo() {
+		return tipo;
+	}
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+	
+}
---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,29 @@
+package util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory = buildSessionFactory();
+	private static ServiceRegistry serviceRegistry;
+	
+	
+	private static SessionFactory buildSessionFactory() {
+		 Configuration configuration = new Configuration();
+		    configuration.configure();
+		    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+		            configuration.getProperties()).build();
+		    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		    return sessionFactory;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
+}
