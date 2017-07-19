5d677170e21a9dfa3cec2fd343af5d17baef074a
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-19 13:38:02 +0000

    spring security

--- branches/exemplo_spring/src/main/java/br/com/escola/configuration/WebApplicationSecurityConfigurerAdapter.java
+++ branches/exemplo_spring/src/main/java/br/com/escola/configuration/WebApplicationSecurityConfigurerAdapter.java
@@ -10,37 +10,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity(debug = false)
 public class WebApplicationSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+	/*Configuração do spring security*/
+	//https://docs.spring.io/spring-security/site/docs/current/reference/html/jc.html
+	
+	//https://docs.spring.io/spring-security/site/docs/current/reference/htmlsingle/
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.formLogin() 
-				.loginPage("/login.jsf") 
-				.loginProcessingUrl("/login.jsf") 
-				.defaultSuccessUrl("/index.jsf") 
-				.failureUrl("/login.xhtml?source=loginError") 
-				.permitAll();
+		http.formLogin().loginPage("/login.jsf").loginProcessingUrl("/login.jsf").defaultSuccessUrl("/index.jsf")
+				.failureUrl("/login.xhtml?source=loginError").permitAll();
 
-		http.authorizeRequests()
-				.antMatchers("/").permitAll() 
-				.antMatchers("/index.html").authenticated() 
-				.antMatchers("/public/**", "/resources/**", "/javax.faces.resource/**").permitAll();
+		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/resources/**", "/javax.faces.resource/**").permitAll();
+		http.logout().logoutUrl("/logout").logoutSuccessUrl("/login.jsf?source=logout").permitAll();
+		
 
-		http.authorizeRequests() 
-				.anyRequest().authenticated();
+		
+		http.authorizeRequests().antMatchers("/aluno.jsf").hasRole("ROOT");
+		http.authorizeRequests().anyRequest().authenticated();
 
-		http.logout() 
-				.logoutUrl("/logout") 
-				.logoutSuccessUrl("/login.jsf?source=logout") 
-				.permitAll();
 		http.csrf().disable();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication() 
-				.withUser("admin") 
-				.password("123456") 
-				.roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("admin").password("123456").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("root").password("root").roles("ROOT");
 	}
 
 }
