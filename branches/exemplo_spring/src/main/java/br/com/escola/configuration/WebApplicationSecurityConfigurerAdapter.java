6664592576be3a11dea167135a9e3cf59336bef6
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-20 16:32:16 +0000

    adição de biliotecas e interação com JSF

--- branches/exemplo_spring/src/main/java/br/com/escola/configuration/WebApplicationSecurityConfigurerAdapter.java
+++ branches/exemplo_spring/src/main/java/br/com/escola/configuration/WebApplicationSecurityConfigurerAdapter.java
@@ -1,8 +1,11 @@
 package br.com.escola.configuration;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Map.Entry;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,17 +13,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import br.com.escola.bean.Aluno;
 import br.com.escola.controle.esp.AlunoBCI;
 
 @Configuration
 @EnableWebSecurity(debug = false)
 public class WebApplicationSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+	private final Properties users = new Properties();
+	private final PasswordEncoder chave = new BCryptPasswordEncoder();
+
 	@Autowired
 	private AlunoBCI controleAluno;
-	
+
 	/* Configuração do spring security */
 	// https://docs.spring.io/spring-security/site/docs/current/reference/html/jc.html
 
@@ -34,8 +40,9 @@ public class WebApplicationSecurityConfigurerAdapter extends WebSecurityConfigur
 		http.authorizeRequests().antMatchers("/").permitAll();
 		http.authorizeRequests().antMatchers("/resources/**", "/javax.faces.resource/**").permitAll();
 		http.logout().logoutUrl("/logout").logoutSuccessUrl("/login.jsf?source=logout").permitAll();
-
-		http.authorizeRequests().antMatchers("/aluno.jsf").hasRole("ROOT");
+//  hasAnyRole("ROOT", "ADMIN")
+		http.authorizeRequests().antMatchers("/aluno.jsf")
+		.hasAnyAuthority("ROOT", "ADMIN");
 		http.authorizeRequests().anyRequest().authenticated();
 
 		http.csrf().disable();
@@ -44,23 +51,33 @@ public class WebApplicationSecurityConfigurerAdapter extends WebSecurityConfigur
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		PasswordEncoder chave = new BCryptPasswordEncoder();
 
-		List<Aluno> list = controleAluno.select();
+		auth.inMemoryAuthentication().withUser("root").password(chave.encode("root")).authorities("ROOT").and()
+				.passwordEncoder(chave);
 		
-		for (Aluno aluno : list) {
-			
-			String senhaAdmin = chave.encode("123456");
-			auth.inMemoryAuthentication().withUser("admin").password(senhaAdmin).roles("ADMIN").and()
-			.passwordEncoder(chave);
+		auth.userDetailsService(inMemoryUserDetailsManager()).passwordEncoder(chave);
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+	//	users.put("thiago", chave.encode("123")+", ROOT, enabled"); 
+		
+
+		InMemoryUserDetailsManager auth = new InMemoryUserDetailsManager(users); 
+		return auth;
+	}
+	
+	/*@Bean
+	public InMemoryUserDetailsManager inMemoryUserDetailsManager(Map<String, String> usersAut) {
+		for (Entry<String, String> entry : usersAut.entrySet()) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue());
+			//chave.encode("123")+", ROOT, enabled"
+			users.put(entry.getKey(), entry.getValue()); 
 		}
 		
-		
-		
-		
-		
-		auth.inMemoryAuthentication().withUser("root").password(chave.encode("root")).roles("ROOT").and()
-		.passwordEncoder(chave);
-	}
+		InMemoryUserDetailsManager auth = new InMemoryUserDetailsManager(users); 
+		return auth;
+	}*/
 
 }
--- branches/exemplo_spring/src/main/java/br/com/escola/mb/AlunoMB.java
+++ branches/exemplo_spring/src/main/java/br/com/escola/mb/AlunoMB.java
@@ -1,4 +1,6 @@
 package br.com.escola.mb;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -6,6 +8,13 @@ import javax.faces.bean.ManagedBean;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import br.com.escola.bean.Aluno;
@@ -20,11 +29,17 @@ public class AlunoMB extends SpringBeanAutowiringSupport {
 	@Autowired
 	private AlunoBCI controle;
 	private List<Aluno> lista;
+
+	@Autowired
+	private UserDetailsManager inMemoryUserDetailsManager;
+
 	
+
 	@PostConstruct
 	public void inicializar() {
 		this.bean = new Aluno();
 		this.lista = controle.select();
+		//this.inMemoryUserDetailsManager = inMemoryUserDetailsManager;
 	}
 
 	public Aluno getBean() {
@@ -42,18 +57,27 @@ public class AlunoMB extends SpringBeanAutowiringSupport {
 	public void setLista(List<Aluno> lista) {
 		this.lista = lista;
 	}
-	
+
 	public void gravar() {
-		//insere o aluno
+		// insere o aluno
 		this.controle.insert(bean);
-		
-		//devolve mensagem de sucesso
-		JSFUtil.adicionarMensagemSucesso(
-				String.format("Aluno %s cadastrado com sucesso.", bean.getNome()));
-		
-		//atualiza informações da tela
+
+		// devolve mensagem de sucesso
+		JSFUtil.adicionarMensagemSucesso(String.format("Aluno %s cadastrado com sucesso.", bean.getNome()));
+
+		// atualiza informações da tela
 		this.bean = new Aluno();
 		this.lista = controle.select();
+		
+		List<GrantedAuthority> l = new ArrayList<GrantedAuthority>();
+		GrantedAuthority s = new SimpleGrantedAuthority("ADMIN");
+		l.add(s);
+		
+		PasswordEncoder chave = new BCryptPasswordEncoder();
+		
+        inMemoryUserDetailsManager.createUser(new User("master", chave.encode("master") , l ));
+
+
 	}
-	
+
 }
