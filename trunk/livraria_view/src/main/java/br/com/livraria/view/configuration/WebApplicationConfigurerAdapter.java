e329c16db64e4f7befbf8234ba9f1cc4f609d26b
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-24 22:21:25 +0000

    livraria view

---
+++ trunk/livraria_view/src/main/java/br/com/livraria/view/configuration/WebApplicationConfigurerAdapter.java
@@ -0,0 +1,17 @@
+package br.com.livraria.view.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebApplicationConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+}
