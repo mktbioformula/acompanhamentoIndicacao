1c2d7a3d94cec314161271b36339aef40921b5aa
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-24 22:35:53 +0000

    entidades da livraria

---
+++ trunk/livraria_controle/src/main/java/br/com/livraria/bean/Autor.java
@@ -0,0 +1,42 @@
+package br.com.livraria.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_autor", sequenceName = "autor_seq", initialValue = 1)
+public class Autor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_autor")
+	private Integer id;
+	private String nome;
+	private String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ trunk/livraria_controle/src/main/java/br/com/livraria/bean/Editora.java
@@ -0,0 +1,49 @@
+package br.com.livraria.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name = "seq_editora", 
+sequenceName = "editora_seq")
+public class Editora {
+	
+	@Id
+	@GeneratedValue(strategy 
+			= GenerationType.SEQUENCE , 
+			generator = "seq_editora")
+	private Integer id;
+	private String nome;
+	private String telefone;
+	private String endereco;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
