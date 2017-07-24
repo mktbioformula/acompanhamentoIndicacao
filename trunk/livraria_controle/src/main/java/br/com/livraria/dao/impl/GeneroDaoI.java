be4fe78e5cb65340a43f1ad0e5105d5ae902a226
esoares <esoares@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-24 23:00:01 +0000

    Genero controle e dao

---
+++ trunk/livraria_controle/src/main/java/br/com/livraria/dao/impl/GeneroDaoI.java
@@ -0,0 +1,10 @@
+package br.com.livraria.dao.impl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.livraria.bean.Genero;
+
+public interface GeneroDaoI extends JpaRepository<Genero, Long>{
+
+	
+}
