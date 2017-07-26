ce4651dd51006f0569e1bbaa6ed60bf1c9f4c128
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-26 22:39:45 +0000

    servidor ws

---
+++ trunk/livraria_ws/src/main/java/br/com/livraria/ws/config/Config.java
@@ -0,0 +1,12 @@
+package br.com.livraria.ws.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "br.com.livraria")
+public class Config {
+
+}
