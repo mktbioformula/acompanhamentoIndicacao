1b8aa4925cadb0e29024707af71d4653a9d0c897
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-07-28 19:22:44 +0000

    cadastro de autores

--- branches/exemplo_spring_mvc/src/main/java/br/com/livraria/view/controladores/UsuarioControlador.java
+++ branches/exemplo_spring_mvc/src/main/java/br/com/livraria/view/controladores/UsuarioControlador.java
@@ -1,15 +1,46 @@
 package br.com.livraria.view.controladores;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.livraria.bean.Autor;
+import br.com.livraria.controle.esp.AutorBCI;
 
 @Controller
 public class UsuarioControlador {
 
-	@RequestMapping("/")
-	public String index(){
-		System.out.println("testando controlador");
-		return "livro";
+	@Autowired
+	private AutorBCI controle;
+
+	private List<Autor> list;
+
+	@PostConstruct
+	public void init() {
+		this.list = controle.select();
 	}
-	
+
+	@RequestMapping("/")
+	public ModelAndView index() {
+		System.out.println("testando controlador");
+		ModelAndView mv = new ModelAndView("livro");
+		mv.addObject("list", this.list);
+
+		return mv;
+	}
+
+	@RequestMapping("/cad_autor")
+	public ModelAndView cadAutor(Autor a) {
+		controle.insert(a);
+		this.init();
+		ModelAndView mv = new ModelAndView("livro");
+		mv.addObject("list", this.list);
+		return mv;
+	}
+
 }
