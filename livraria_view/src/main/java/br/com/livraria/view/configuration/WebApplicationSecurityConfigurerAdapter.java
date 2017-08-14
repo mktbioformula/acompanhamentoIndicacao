e57538b0f2a13a208d32a1d3ee5d7a31d1d72998
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-08-14 23:26:35 +0000

    config

--- livraria_view/src/main/java/br/com/livraria/view/configuration/WebApplicationSecurityConfigurerAdapter.java
+++ livraria_view/src/main/java/br/com/livraria/view/configuration/WebApplicationSecurityConfigurerAdapter.java
@@ -2,6 +2,7 @@ package br.com.livraria.view.configuration;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,11 +13,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import br.com.livraria.controle.esp.AutorBCI;
+
 @Configuration
 @EnableWebSecurity(debug = false)
 public class WebApplicationSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+	
+	@Autowired
+	private AutorBCI autorControle;
 	private final Properties users = new Properties();
+
+	
 	private final PasswordEncoder chave = new BCryptPasswordEncoder();
 
 	/* Configuração do spring security */
@@ -26,46 +34,64 @@ public class WebApplicationSecurityConfigurerAdapter extends WebSecurityConfigur
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.formLogin().loginPage("/login.jsf").loginProcessingUrl("/login.jsf").defaultSuccessUrl("/index.jsf")
-				.failureUrl("/login.jsf?source=loginError").permitAll();
-
-		http.authorizeRequests().antMatchers("/").permitAll();
-		http.authorizeRequests().antMatchers("/resources/**", "/javax.faces.resource/**").permitAll();
-		http.logout().logoutUrl("/logout").logoutSuccessUrl("/login.jsf?source=logout").permitAll();
-		// hasAnyRole("ROOT", "ADMIN")
-		http.authorizeRequests().antMatchers("/aluno.jsf").hasAnyAuthority("ROOT", "ADMIN");
+		// definindo página delogin
+		http.formLogin().loginPage("/login.jsf");
+		http.formLogin().loginProcessingUrl("/login.jsf");
+		// definindo url de acesso bem sucedido
+		http.formLogin().defaultSuccessUrl("/index.jsf");
+		// falha ao logar
+		http.formLogin().failureUrl("/login.jsf?source=loginError");
+		// liberando acesso total a página de login
+		http.formLogin().permitAll();
+		
+		// liberando acesso a pasta resources
+		http.authorizeRequests()
+		.antMatchers("/resources/**", "/javax.faces.resource/**")
+		.permitAll();
+		// liberando acesso a página de logout
+		http.logout()
+		.logoutUrl("/logout")
+		.logoutSuccessUrl("/login.jsf?source=logout")
+		.permitAll();
+		
+		// para acessar a página de aluno.jsf tenho q ter o perfil de ADMIN ou ROOT
+		http.authorizeRequests()
+		.antMatchers("/aluno.jsf")
+		.hasAnyAuthority("ROOT", "ADMIN");
+		
+		// para qualquer outra página o usuário deve estar autenticado
 		http.authorizeRequests().anyRequest().authenticated();
-
+		
 		http.csrf().disable();
 	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.inMemoryAuthentication().withUser("root").password(chave.encode("root")).authorities("ROOT").and()
-				.passwordEncoder(chave);
-
-		auth.userDetailsService(inMemoryUserDetailsManager()).passwordEncoder(chave);
+	
+		auth.inMemoryAuthentication()
+		.withUser("root")
+		.password(chave.encode("root"))
+		.authorities("ADMIN").and()
+		.passwordEncoder(chave);
+		;
+		
+		
+		auth.userDetailsService(inMemoryUserDetailsManager())
+		.passwordEncoder(chave);
+		
+		
 	}
 
 	@Bean
 	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-		// users.put("thiago", chave.encode("123")+", ROOT, enabled");
+		
+		
+		users.put("thiago", chave.encode("123")+", ROOT");
 
 		InMemoryUserDetailsManager auth = new InMemoryUserDetailsManager(users);
 		return auth;
 	}
 
-	/*
-	 * @Bean public InMemoryUserDetailsManager
-	 * inMemoryUserDetailsManager(Map<String, String> usersAut) { for
-	 * (Entry<String, String> entry : usersAut.entrySet()) {
-	 * System.out.println(entry.getKey()); System.out.println(entry.getValue());
-	 * //chave.encode("123")+", ROOT, enabled" users.put(entry.getKey(),
-	 * entry.getValue()); }
-	 * 
-	 * InMemoryUserDetailsManager auth = new InMemoryUserDetailsManager(users);
-	 * return auth; }
-	 */
-
+	
 }
