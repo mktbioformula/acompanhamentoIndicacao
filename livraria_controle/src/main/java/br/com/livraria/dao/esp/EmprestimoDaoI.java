8c659f19c1e8810a438f730496cfb8357a135c6e
turma3way <turma3way@a49b76a6-5d6c-4916-8851-05b5eae57067>
2017-08-15 00:54:06 +0000

    init

--- livraria_controle/src/main/java/br/com/livraria/dao/esp/EmprestimoDaoI.java
+++
@@ -1,9 +0,0 @@
-package br.com.livraria.dao.esp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.livraria.bean.Emprestimo;
-
-public interface EmprestimoDaoI extends JpaRepository<Emprestimo, Long>{
-
-}
